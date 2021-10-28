9e88850dae5841b8bad1ddb3e37ea0052482be67
chrisdaaz <chris-diaz@northwestern.edu>
2021-10-28 13:39:41 -0500

    finish script

--- documents.sh
+++ documents.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 # helps replace the PDF viewer with a local pdf-js instance
 
-for FILE in content/documents/*.md; do
-    pdf_file="$(basename "${FILE%%.*}")"
-    printf "\n\n{{< embed-pdf url="https://florencekelley.northwestern.edu/pdfs/$pdf_file.pdf" >}}" >> $FILE
-done;
+add_shortcode() {
+    for FILE in content/documents/*.md; do
+        pdf_file="$(basename "${FILE%%.*}")"
+        printf "\n\n{{< embed-pdf url="https://florencekelley.northwestern.edu/pdfs/$pdf_file.pdf" >}}#" >> $FILE
+    done;
+}
+
+remove_iframe() {
+    for FILE in content/documents/*.md; do
+        sed -i '/iframe/d' $FILE
+    done;
+}
